cdabf588bcc10423c9be97a6dde43b389c35dc71
Mark Calla <markcalla@byui.edu>
2024-05-30 20:27:39 -0500

    Don't forget 'bout the require statement for the static file

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController")
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -22,9 +23,12 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 //Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+/*app.get("/", function(req, res){
+//  res.render("index", {title: "Home"})
+})*/
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
